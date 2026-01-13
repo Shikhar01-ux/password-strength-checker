c19a7b8a8e0dac9de7c0e2922196dc8a888ff792
Shikhar01-ux <yadavshikhar613@gmail.com>
2026-01-13 21:01:08 +0530

    Defining check password strenfth function

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    feedback = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Use at least 8 characters")
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        feedback.append("Add uppercase letters")
+
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("Add lowercase letters")
+
+    if re.search(r"[0-9]", password):
+        score += 1
+    else:
+        feedback.append("Add numbers")
+
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+    else:
+        feedback.append("Add special characters")
+
+    return score, feedback
